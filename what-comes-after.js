0dd5e7d130d32723e75740c479ee4e884f170936
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-27 09:20:30 -0500

    Final for code challenge

--- what-comes-after.js
+++ what-comes-after.js
@@ -1,11 +1,9 @@
 const comes_after = (str, letter) => {
-	console.log(str, letter)
 	let answer = ''
 	for (let index = 0; index < str.length; index++) {
 		const element = str.toLowerCase()[index]
 		if (element === letter && str[index + 1] && str[index + 1].match(/[A-Za-z]/)) {
-			answer.concat(str[index + 1])
-			console.log(str[index + 1])
+			answer = `${answer}${str[index + 1]}`
 		}
 	}
 	return answer
