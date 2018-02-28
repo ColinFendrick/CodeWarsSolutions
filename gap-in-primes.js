8999378a7fe971b507bb45c4404d067eaa34ef2a
Colin Fendrick <colin.fendrick@gmail.com>
2018-02-28 08:47:46 -0500

    Updated gap in primes to reflect instructions

--- gap-in-primes.js
+++ gap-in-primes.js
@@ -1,16 +1,27 @@
-const isPrime = int => {
-	for (let index = 2; index < int; index++) {
-		if (int % index === 0) {
-			return false
-		}
-	}
-	return int > 1
-}
-
 const gap = (gap, m, n) => {
+	const isPrime = int => {
+		for (let index = 2; index < int; index++) {
+			if (int % index === 0) {
+				return false
+			}
+		}
+		return true
+	}
+
+	const checkForNoPrimes = (start, stop) => {
+		for (let index = start; index < stop; index++) {
+			if (isPrime(index)) {
+				return false
+			}
+		}
+		return true
+	}
+
 	for (let index = m; index <= n; index++) {
 		if (isPrime(index) && isPrime(index + gap)) {
-			return [index, index + gap]
+			if (checkForNoPrimes(index + 1, index + gap)) {
+				return [index, index + gap]
+			}
 		}
 	}
 	return null
