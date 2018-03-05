70bcfe0e3b60a5aaa9c51f6775165e72991e1cfa
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-05 11:19:00 -0500

    A little refactoring

--- josephus-permutation.js
+++ josephus-permutation.js
@@ -2,14 +2,13 @@ const josephus = (items, k) => {
   const result = []
   let counter = 0
   let index = 0
-  while (items.length > 0) {
-    index = index % items.length
+  while (items.length) {
     if (++counter === k) {
       result.push(items.splice(index, 1)[0])
       counter = 0
       index--
     }
-    index++
+    index = (index + 1) % items.length
   }
   return result
 }
