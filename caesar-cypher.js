50496ce450458ecd941be053bdc4f059930991ba
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-14 13:56:41 -0400

    cypher working, need to break it up

---
+++ caesar-cypher.js
@@ -0,0 +1,18 @@
+const movingShift = (str, shift) => {
+	let output = ''
+	for (let i = 0; i < str.length; i++) {
+		let letter = str[i]
+		if (letter.match(/[a-z]/i)) {
+			const code = str.charCodeAt(i)
+			if ((code >= 65) && (code <= 90)) {
+				letter = String.fromCharCode(((code - 65 + shift) % 26) + 65)
+			}
+			else if ((code >= 97) && (code <= 122)) {
+				letter = String.fromCharCode(((code - 97 + shift) % 26) + 97)
+			}
+		}
+		output += letter
+		shift++
+	}
+	return output
+}
