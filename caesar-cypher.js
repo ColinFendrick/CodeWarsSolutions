c7bc8413dac7ca652f2bc9a134e51d1e73ad9b4a
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-14 15:55:35 -0400

    finally...

--- caesar-cypher.js
+++ caesar-cypher.js
@@ -16,7 +16,7 @@ const movingShift = (str, shift) => {
 	}
 	const rangeEnd = Math.ceil(output.length/5)
 	const answer = output.match(new RegExp('.{1,' + rangeEnd + '}', 'g'))
-	return answer.length === 5 ? answer : answer.push('')
+	return answer.length === 5 ? answer : answer.push(' ')
 }
 
 const demovingShift = (input, shift) => {
@@ -29,7 +29,7 @@ const demovingShift = (input, shift) => {
 			if ((code >= 65 - shift) && (code <= 90)) {
 				current = String.fromCharCode(((code - 65 - shift + (26 * input.length)) % 26) + 65)
 			}
-			else if ((code >= 97) && (code <= 122)) {
+			else if ((code >= 97 - shift) && (code <= 122)) {
 				current = String.fromCharCode(((code - 97 - shift + (26 * input.length)) % 26) + 97)
 			}	
 		}
