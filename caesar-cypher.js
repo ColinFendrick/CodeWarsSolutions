a2255b5a78d9c15d5c3e00d4730998e60437da66
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-14 14:37:42 -0400

    ugh...

--- caesar-cypher.js
+++ caesar-cypher.js
@@ -1,19 +1,40 @@
 const movingShift = (str, shift) => {
 	let output = ''
 	for (let i = 0; i < str.length; i++) {
-		let letter = str[i]
-		if (letter.match(/[a-z]/i)) {
+		let current = str[i]
+		if (current.match(/[a-z]/i)) {
 			const code = str.charCodeAt(i)
 			if ((code >= 65) && (code <= 90)) {
-				letter = String.fromCharCode(((code - 65 + shift) % 26) + 65)
+				current = String.fromCharCode(((code - 65 + shift) % 26) + 65)
 			}
 			else if ((code >= 97) && (code <= 122)) {
-				letter = String.fromCharCode(((code - 97 + shift) % 26) + 97)
+				current = String.fromCharCode(((code - 97 + shift) % 26) + 97)
 			}
 		}
-		output += letter
+		output += current
 		shift++
 	}
 	const rangeEnd = Math.ceil(output.length/5)
-	return output.match(new RegExp('.{1,' + rangeEnd + '}', 'g'))
+	const answer = output.match(new RegExp('.{1,' + rangeEnd + '}', 'g'))
+	return answer.length === 5 ? answer : answer.push('')
+}
+
+const demovingShift = (input, shift) => {
+	let output = ''
+	let str = input.join()
+	for (let i =0; i < str.length; i++) {
+		let current = str[i]
+		if (current.match(/[a-x]/i)) {
+			const code = str.charCodeAt(i)
+			if ((code >= 65) && (code <= 90)) {
+				current = String.fromCharCode(((code - 65 - shift) % 26) + 65)
+			}
+			else if ((code >= 97) && (code <= 122)) {
+				current = String.fromCharCode(((code - 97 - shift) % 26) + 97)
+			}
+		}
+		output += current
+		shift++
+	}
+	return output
 }
