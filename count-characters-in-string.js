4c72f9e0daaefa222ba60bdad35c793c79839fa4
Colin Fendrick <colin.fendrick@gmail.com>
2018-03-16 09:45:03 -0400

    Done

--- count-characters-in-string.js
+++ count-characters-in-string.js
@@ -1,7 +1,7 @@
-const count = str => {
+function count(string) {
 	let answer = {}
-	for (let i = 0; i < str.length; i++) {
-		answer[str[i]]++
+	for (let i = 0; i < string.length; i++) {
+		!answer[string[i]] ? answer[string[i]] = 1 : answer[string[i]]++
 	}
-	return answer
+	return (answer)
 }
