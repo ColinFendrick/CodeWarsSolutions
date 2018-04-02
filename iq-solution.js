825a66989b0adc2ad34424f1752e196ed837df06
Colin Fendrick <cfendrick@Macs-iMac-2.local>
2018-04-02 09:12:27 -0400

    6 kyu

---
+++ iq-solution.js
@@ -0,0 +1,8 @@
+const iqTest = numbers => {
+  numbers = numbers.split(" ").map(el => parseInt(el))
+
+  const odd = numbers.filter(el => el % 2 === 1)
+  const even = numbers.filter(el => el % 2 === 0)
+
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1)
+}
