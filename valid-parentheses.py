0d5542e88a96eb875f8a21a46df3eb42c78aa1e4
Colin Fendrick <colin.fendrick@gmail.com>
2018-04-03 08:30:08 -0400

    6kyu

--- valid-parentheses.py
+++ valid-parentheses.py
@@ -1,12 +1,7 @@
 def valid_parentheses(string):
-  balance = 0
-  for item in string:
-    if item == "(":
-      balance = balance + 1
-    if item == ")":
-      balance = balance - 1
-    if balance < 0:
-      return False
-  if balance != 0:
-    return False
-  return True
+    balance = 0
+    for char in string:
+        if char == '(': balance += 1
+        if char == ')': balance -= 1
+        if balance < 0: return False
+    return True if balance == 0 else False
