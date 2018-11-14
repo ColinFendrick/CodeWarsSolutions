a92e44baf717204ce9eb155cd8b45fbc0cf6365d
Colin Fendrick <colin.fendrick@gmail.com>
2018-11-14 08:23:29 -0500

    6 kyu

---
+++ ip-validate.js
@@ -0,0 +1,16 @@
+const isValidIP = str => {
+
+	const octetCheck = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)$/
+	const splitStr = str.split('.')
+	if (splitStr.length !== 4) { 
+		return false 
+	}
+  
+	for (let i in splitStr) {
+		if (!octetCheck.test(splitStr[i])) {
+			return false
+		}
+	}
+
+	return true  
+}
