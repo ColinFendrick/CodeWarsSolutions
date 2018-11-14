dc187f79e158af8f04f0a127f42cea5544a7af84
Colin Fendrick <colin.fendrick@gmail.com>
2018-11-14 08:25:07 -0500

    6 kyu way better solution

--- ip-validate.js
+++ ip-validate.js
@@ -1,16 +1,18 @@
-const isValidIP = str => {
+// const isValidIP = str => {
 
-	const octetCheck = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)$/
-	const splitStr = str.split('.')
-	if (splitStr.length !== 4) { 
-		return false 
-	}
+// 	const octetCheck = /^(25[0-5]|2[0-4]\d|1\d\d|[1-9]\d|\d)$/
+// 	const splitStr = str.split('.')
+// 	if (splitStr.length !== 4) { 
+// 		return false 
+// 	}
   
-	for (let i in splitStr) {
-		if (!octetCheck.test(splitStr[i])) {
-			return false
-		}
-	}
+// 	for (let i in splitStr) {
+// 		if (!octetCheck.test(splitStr[i])) {
+// 			return false
+// 		}
+// 	}
 
-	return true  
-}
+// 	return true  
+// }
+
+const isValidIP = str => /^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$/.test(str)
