2e8602df7d80437008e72edeea956f94c5d581e7
Colin Fendrick <colin.fendrick@gmail.com>
2019-02-26 09:29:25 -0500

    6 kyu

---
+++ unique-in-order.js
@@ -0,0 +1,12 @@
+const uniqueInOrder = iterable => {
+  let result = [];
+  let last;
+
+  for (let i = 0; i < iterable.length; i++) {
+    if (iterable[i] !== last) {
+      result.push(last = iterable[i])
+    }
+  };
+
+  return result
+}
