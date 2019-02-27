b52571d424bdbc97d39e31f735ee96ffce05c707
Colin Fendrick <colin.fendrick@gmail.com>
2019-02-27 11:02:38 -0500

    6 kyu

---
+++ your-order-please.js
@@ -0,0 +1,17 @@
+const order = words => {
+  if (words === '') {
+    return ''
+  }
+  let stringOfWords = words.split(' ')
+  let answer = []
+  let j = 1
+  for (let i = 0; i < stringOfWords.length; i++) {
+    if (stringOfWords[i].indexOf(j) > -1) {
+      answer.push(words[i])
+      j++
+      i=-1
+    }
+  }
+  return answer.join(' ')
+}
+
