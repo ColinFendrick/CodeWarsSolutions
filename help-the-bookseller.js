c2ac2b971a197b7703e7c82119168d5772d913a3
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-04 09:54:44 -0400

    6kyu

---
+++ help-the-bookseller.js
@@ -0,0 +1,8 @@
+const stockList = (a, b) => {
+  if (!a.length) { return '' };
+  const res = b.reduce((obj, i) => {
+    obj[i]=0; return obj 
+  }, {});
+  a.forEach(el => b.includes(el[0]) ? res[el[0]]+= +(el.match(/\d+/)[0]) : undefined);
+  return b.map(el => `(${el} : ${res[el]})`).join(' - ');
+};
