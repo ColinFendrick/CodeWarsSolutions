60f9d26d078a7153cd0c3e8037bdf8d2f4eb0a69
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-05 10:52:46 -0400

    5 kyu

--- simple-pig-latin.js
+++ simple-pig-latin.js
@@ -1,14 +1,13 @@
-let pigIt = (str) => {
-	return str.split(' ').map((word) => word.substr(1) + word.substr(0, 1) + 'ay').join(' ')
-}
+const pigIt = str =>
+	str.split(' ').map((word) => word.substr(1) + word.substr(0, 1) + 'ay').join(' ');
 
 //Refactor
-pigIt = (str) => {
-	let words = str.split(' ')
-	let vowels = ['a', 'e', 'i', 'o', 'u']
-	let newStrings = []
-	for (var i = 0; i < words.length; i++) {
-		newStrings.push(words[i].substr(1) + words[i].substr(0, 1) + 'ay')
-	}
-	return newStrings.join(' ')
-}
+const pigIt = str => {
+	let words = str.split(' ');
+	let vowels = ['a', 'e', 'i', 'o', 'u'];
+	let newStrings = [];
+	for (let i = 0; i < words.length; i++) {
+		newStrings.push(words[i].substr(1) + words[i].substr(0, 1) + 'ay');
+	};
+	return newStrings.join(' ');
+};
