b2cb510a61929469191d930f74a1804a3aaeb158
Colin Fendrick <colin.fendrick@gmail.com>
2019-04-11 09:20:33 -0400

    6kyu

---
+++ reverse-or-rotate.js
@@ -0,0 +1,15 @@
+const digitSum = str =>
+  str.split("").map(Number).reduce((pre, el) => pre + el, 0);
+
+const revrot = (str, sz) => {
+  if (sz <= 0 || str === "" || sz > str.length) return "";
+
+  const arr=[];
+  for (let i = 0; i < Math.floor(str.length / sz); i++) {
+    arr.push(str.slice(i * sz, (i+1) * sz));
+  };
+
+  return arr.map(el =>
+    digitSum(el) % 2 === 0 ? el.split("").reverse().join("") : el.slice(1) + el[0]
+  ).join(""); 
+};
