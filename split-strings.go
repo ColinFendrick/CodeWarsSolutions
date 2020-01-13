2a0065f5523bf9a976248a2867e778aa70aa6693
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-13 12:24:59 -0500

    6kyu

---
+++ split-strings.go
@@ -0,0 +1,18 @@
+package kata
+
+// Solution ...
+func Solution(str string) []string {
+	result := []string{}
+
+	for len(str) != 0 {
+		if len(str) > 1 {
+			result = append(result, str[0:2])
+			str = str[2:]
+		} else if len(str) == 1 {
+			result = append(result, str[0:1]+"_")
+			str = str[1:]
+		}
+	}
+
+	return result
+}
