2173d78cbc19dc57b7c6ff6069da0ef9c0a188ac
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-13 12:05:31 -0500

    6kyu

---
+++ multiples-of-3-or-5.go
@@ -0,0 +1,12 @@
+package kata
+
+// Multiple3And5 ...
+func Multiple3And5(number int) int {
+	result := 0
+	for i := 3; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			result += i
+		}
+	}
+	return result
+}
