aaffc973d8ddf307294f411933f920a91a0a96f2
Colin Fendrick <colin.fendrick@gmail.com>
2020-01-13 14:22:10 -0500

    6kyu

---
+++ ip-validate.go
@@ -0,0 +1,22 @@
+package kata
+
+import (
+	"strconv"
+	"strings"
+)
+
+// Is_valid_ip
+func Is_valid_ip(ip string) bool {
+	nums := strings.Split(ip, ".")
+	if len(nums) != 4 {
+		return false
+	}
+	for _, value := range nums {
+		n, err := strconv.Atoi(value)
+		if err != nil || n < 0 || n > 255 || len(value) != len(strconv.Itoa(n)) {
+			return false
+		}
+	}
+
+	return true
+}
