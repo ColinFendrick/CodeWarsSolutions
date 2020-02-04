36354e415eff1c27411cb69c51614e6b7781bef6
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-04 06:38:52 -0500

    6kyu

---
+++ digital-roots.go
@@ -0,0 +1,15 @@
+package kata
+
+// DigitalRoot ...
+func DigitalRoot(n int) int {
+	if n < 10 {
+		return n
+	}
+
+	result := 0
+	for n != 0 {
+		result += n % 10
+		n /= 10
+	}
+	return DigitalRoot(result)
+}
