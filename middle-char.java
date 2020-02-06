eb5a4ddb1c73307f104536d4d737a0593fd8ca04
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-06 07:58:24 -0500

    7kyu

---
+++ middle-char.java
@@ -0,0 +1,8 @@
+class Kata {
+	public static String getMiddle(String word) {
+		int l = word.length();
+		if (l%2==1)
+			return word.substring(l/2,(l/2)+1);
+		return word.substring(l/2-1,l/2+1);
+	}
+}
