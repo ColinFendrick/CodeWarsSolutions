7e0add8c191ff94b724216ae08b1cb619b44d46d
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-20 06:12:58 -0500

    7kyu

---
+++ DontGiveMeFive.java
@@ -0,0 +1,13 @@
+public class Kata {
+	public static int dontGiveMeFive(int start, int end) {
+		int count = 0;
+		for (int i = start; i <= end; i++)
+			count += containsFive(i)?0:1;
+		return count;
+	}
+
+	private static boolean containsFive(int i) {
+		return String.valueOf(i).contains("5");
+	}
+}
+
