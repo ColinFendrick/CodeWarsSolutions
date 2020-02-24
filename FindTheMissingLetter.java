3620cee4a8957a210b5ddd984b4a44db4bd263d9
Colin Fendrick <colin.fendrick@gmail.com>
2020-02-24 05:58:11 -0500

    6kyu

---
+++ FindTheMissingLetter.java
@@ -0,0 +1,9 @@
+public class FindTheMissingLetter {
+  public static char findMissingLetter(char[] array) {
+    for (int i = 1; i < array.length; i++) {
+      if (array[i] - array[i - 1] != 1)
+        return (char) (array[i] - 1);
+    }
+    return '?';
+  }
+}
