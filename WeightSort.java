d18e656ae6671feffaf9a0b17187edbd91a18fc5
Colin Fendrick <colin.fendrick@gmail.com>
2020-04-21 10:34:56 -0400

    5kyu

---
+++ WeightSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class WeightSort {
+
+	public static String orderWeight(String strng) {
+		String weights[] = strng.split(" ");
+
+		Arrays.sort(weights, new java.util.Comparator<String>() {
+			@Override
+			public int compare(String s1, String s2) {
+				int v1 = 0;
+				for (int i = 0; i < s1.length(); i++) {
+					int valor = (int)s1.charAt(i)-48;
+					if (valor <= 9) {
+						v1 += valor;
+					}
+				}
+				int v2 = 0;
+				for (int i = 0; i < s2.length(); i++) {
+					int valor = (int)s2.charAt(i)-48;
+					if (valor <= 9) {
+						v2 += valor;
+					}
+				}
+				if (v1-v2 == 0)
+					return s1.compareTo(s2);
+
+				return v1 - v2;
+			}
+		});
+		return String.join(" ", weights);
+	}
+}
