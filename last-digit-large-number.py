00816696e300432fbf636f903b1e3127cd6d12df
Colin Fendrick <colin.fendrick@gmail.com>
2020-05-01 06:10:57 -0400

    refactor

--- last-digit-large-number.py
+++ last-digit-large-number.py
@@ -1,4 +1,8 @@
+# def last_digit(n1, n2):
+#     if not n1 or not n2:
+#         return 1
+#     return int(str(n1 ** (n2 % 4 if n2 % 4 else 4))[-1])
+
+
 def last_digit(n1, n2):
-    if not n1 or not n2:
-        return 1
-    return int(str(n1 ** (n2 % 4 if n2 % 4 else 4))[-1])
+    return pow(n1, n2, 10)
