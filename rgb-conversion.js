6395c129865ec4986d2d76be12f44cc938e83c33
Colin Fendrick <colin.fendrick@gmail.com>
2020-05-07 06:38:22 -0400

    5kyu

---
+++ rgb-conversion.js
@@ -0,0 +1,9 @@
+const rgb = (r, g, b) => {
+  const toHex = c => {
+    if (c > 255) return "FF"
+    else if (c < 0) return "00"
+    else return c.toString(16).padStart(2, "0").toUpperCase()
+  }
+
+  return toHex(r) + toHex(g) + toHex(b)
+}
