880f9ada9d7ba08812235f6b976e3d5548e3f1ae
Colin Fendrick <colin.fendrick@gmail.com>
2020-05-15 06:13:58 -0400

    7kyu

---
+++ growth-of-a-population.go
@@ -0,0 +1,11 @@
+package kata
+
+// NbYear ...
+func NbYear(p0 int, percent float64, aug int, p int) int {
+	var count int
+	for p0 < p {
+		p0 = p0 + int(float64(p0)*percent/100) + aug
+		count++
+	}
+	return count
+}
