54d6aeab77b77faa570d90181cb3023f95fa1fd8
Colin Fendrick <colin.fendrick@gmail.com>
2020-08-21 09:14:05 -0400

    7kyu

---
+++ brand-name-generator.go
@@ -0,0 +1,10 @@
+package kata
+
+import "strings"
+
+func bandNameGenerator(word string) string {
+	if word[0] == word[len(word)-1] {
+		return strings.Title(word + word[1:])
+	}
+	return "The " + strings.Title(word)
+}
