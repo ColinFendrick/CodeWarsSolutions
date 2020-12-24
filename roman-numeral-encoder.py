22d512c6bec7438287257068726c1b2c307bac95
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-24 05:27:47 -0800

    4kyu

--- roman-numeral-encoder.py
+++ roman-numeral-encoder.py
@@ -1,4 +1,4 @@
-ROMAN = list(
+KEY = list(
 	zip(
 		('I','IV','V','IX','X','XL','L','XC','C','CD','D','CM','M'), (1,4,5,9,10,40,50,90,100,400,500,900,1000)
 	)
@@ -6,7 +6,7 @@ ROMAN = list(
 
 def solution(data):
 	result = ''
-	for i in ROMAN[::-1]:
+	for i in KEY[::-1]:
 		if i[1] <= data:
 			while data - i[1] >= 0:
 				data = data - i[1]
