53c5b13d415d047e5d2052e8593e06038c1158f4
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-24 08:27:16 -0800

    6kyu

---
+++ is-prime/src/lib.rs
@@ -0,0 +1,7 @@
+use num::integer::sqrt;
+
+fn is_prime(x: i64) -> bool {
+    if x == 2 {return true};
+    if x < 2 || x % 2 == 0 {return false};
+    (3..).step_by(2).take_while(|i| i*i <= sqrt(x)).all(|i| x % i != 0)
+}
