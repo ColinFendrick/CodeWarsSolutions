d2bd305f62856dba20e25078e3d54ba872ee6a6d
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-30 05:10:55 -0800

    alternative sol

--- diophantine.py
+++ diophantine.py
@@ -12,3 +12,7 @@ def sol_equa(n):
 				res.append([x, y])
 
 	return res
+
+# def sol_equa(n):
+#   return [[(n/i + i)/2,(n/i - i)/4] for i in range(1, int(n ** 0.5) + 1)
+#   if n % i == 0 and (n/i + i) % 2 ==0 and (n/i - i) % 4 ==0]
