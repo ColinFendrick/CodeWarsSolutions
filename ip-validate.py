7cfec4470715dc3de09039e20c1b29f8a890b15b
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-01 08:43:35 -0800

    6kyu

--- ip-validate.py
+++ ip-validate.py
@@ -1,6 +1,8 @@
 import re
 
 def is_valid_IP(str):
-  return True if re.search(
-    '^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$', str
-  ) else False
+  return bool(
+    re.search(
+      '^(([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.(?!$)|$)){4}$', str
+    )
+  )
