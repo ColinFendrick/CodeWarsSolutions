dc575162ba0d23211db38ce4325de068f949f603
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-02 04:52:54 -0800

    6kyu

--- camel-case.py
+++ camel-case.py
@@ -1,1 +1,8 @@
-camel_case = lambda string: ''.join(x.capitalize() for x in string.split())
+# to_camel_case = lambda string: ''.join(x.capitalize() for x in string.split())
+
+import re
+word_regex_pattern = re.compile("[^A-Za-z]+")
+
+def to_camel_case(st):
+  words = word_regex_pattern.split(st)
+  return "".join(w.lower() if i is 0 and w.islower() else w.title() for i, w in enumerate(words))
