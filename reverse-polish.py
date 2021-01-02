2c01e603ba108799184a262be0753ccd184c1875
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-02 09:33:50 -0800

    refactor

--- reverse-polish.py
+++ reverse-polish.py
@@ -1,25 +1,46 @@
-import re
+# import re
 
-def calc(expr):
-  tokens = expr.split(' ')
-  numberStack = []
-  for token in tokens:
-    if re.match('\d+.?\d*?', token):
-      numberStack.append(float(token))
-    else:
-      if len(numberStack) > 0:
-        b = numberStack.pop()
-      if len(numberStack) > 0:
-        a = numberStack.pop
+# def calc(expr):
+#   tokens = expr.split(' ')
+#   numberStack = []
+#   for token in tokens:
+#     if re.match('\d+.?\d*?', token):
+#       numberStack.append(float(token))
+#     else:
+#       if len(numberStack) > 0:
+#         b = numberStack.pop()
+#       # if len(numberStack) > 0:
+#         a = numberStack.pop
 
-      if (token == '+'):
-        numberStack.append(a + b)
-      if (token == '-'):
-        numberStack.append(a - b)
-      if (token == '*'):
-        numberStack.append(a * b)
-      if (token == '/'):
-        numberStack.append(a / b)
+#         print(a, b)
+
+#       if (token == '+'):
+#         numberStack.append(a + b)
+#       if (token == '-'):
+#         numberStack.append(a - b)
+#       if (token == '*'):
+#         numberStack.append(a * b)
+#       if (token == '/'):
+#         numberStack.append(a / b)
         
       
-  return numberStack[0] if len(numberStack) > 0 else 0
+#   return numberStack[0] if len(numberStack) > 0 else 0
+
+import operator
+
+def calc(expr):
+  operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.div,
+  }
+  stack = []
+  for s in expr.split():
+    op = operators.get(s)
+    if op is not None:
+      b, a = stack.pop(), stack.pop()
+      stack.append(op(a, b))
+    else:
+      stack.append(float(s))
+  return stack[-1] if stack else 0
