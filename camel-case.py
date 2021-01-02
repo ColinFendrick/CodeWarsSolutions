643b10b225ce2d78269e6ce9adca9453d6239a4e
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-02 04:54:35 -0800

    Alternate take

--- camel-case.py
+++ camel-case.py
@@ -1,8 +1,10 @@
 # to_camel_case = lambda string: ''.join(x.capitalize() for x in string.split())
 
 import re
-word_regex_pattern = re.compile("[^A-Za-z]+")
 
-def to_camel_case(st):
-  words = word_regex_pattern.split(st)
+def to_camel_case(s):
+  words = re.compile("[^A-Za-z]+").split(s)
   return "".join(w.lower() if i is 0 and w.islower() else w.title() for i, w in enumerate(words))
+
+# def to_camel_case(s):
+#   return s[0] + s.title().translate(None, "-_")[1:] if s else s
