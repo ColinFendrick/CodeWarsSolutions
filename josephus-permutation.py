d44225442b3db734d88998fab3a15f11e0524905
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-03 04:52:18 -0800

    5kyu

--- josephus-permutation.py
+++ josephus-permutation.py
@@ -1,8 +1,8 @@
 def josephus(items, k):
-    index = 0
-    items_copy = list(items)
-    result = []
-    while items_copy:
-        index = (index + (k - 1)) % len(items_copy)
-        result.append(items_copy.pop(index))
-    return result
+	index = 0
+	items_copy = list(items)
+	result = []
+	while items_copy:
+		index = (index + (k - 1)) % len(items_copy)
+		result.append(items_copy.pop(index))
+	return result
