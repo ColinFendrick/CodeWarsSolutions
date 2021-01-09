7e1b2992746709e8f309ac85628c194a3798c7ee
Colin Fendrick <colin.fendrick@gmail.com>
2021-01-09 09:12:27 -0800

    refactor

--- tally-it-up.js
+++ tally-it-up.js
@@ -1,29 +1,31 @@
-const scoreToTally = score => {
-	const count = Math.floor(score / 5);
-	const leftover = score % 5;
+// const scoreToTally = score => {
+// 	const count = Math.floor(score / 5);
+// 	const leftover = score % 5;
 
-	let res = '';
-	for (let i = 0; i < count; i++)
-		res += 'e <br>';
+// 	let res = '';
+// 	for (let i = 0; i < count; i++)
+// 		res += 'e <br>';
 
-	switch (leftover) {
-	case 0:
-		break;
-	case 1:
-		res += 'a';
-		break;
-	case 2:
-		res += 'b';
-		break;
-	case 3:
-		res += 'c';
-		break;
-	case 4:
-		res += 'd';
-		break;
-	default:
-		break;
-	}
+// 	switch (leftover) {
+// 	case 0:
+// 		break;
+// 	case 1:
+// 		res += 'a';
+// 		break;
+// 	case 2:
+// 		res += 'b';
+// 		break;
+// 	case 3:
+// 		res += 'c';
+// 		break;
+// 	case 4:
+// 		res += 'd';
+// 		break;
+// 	default:
+// 		break;
+// 	}
 
-	return res;
-};
+// 	return res;
+// };
+
+const scoreToTally = score => 'e <br>'.repeat((score / 5) | 0) + ['', 'a', 'b', 'c', 'd'][score % 5];
